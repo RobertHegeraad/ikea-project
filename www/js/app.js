928633c37152498caf98c636f5170edd03ada6be
robert <roberthegeraad@gmail.com>
2015-05-27 16:10:29 +0200

    bluetooth serial connect

--- www/js/app.js
+++ www/js/app.js
@@ -81,8 +81,8 @@ angular.module('starter', ['ionic', 'controllers'])
     }
   })
 
-  // $urlRouterProvider.otherwise("/start");
-  $urlRouterProvider.otherwise("/debug");
+  $urlRouterProvider.otherwise("/start");
+  // $urlRouterProvider.otherwise("/debug");
 })
 
 /*
@@ -183,9 +183,12 @@ angular.module('starter', ['ionic', 'controllers'])
           bluetoothSerial.enable(function(data) {
             console.log('BL init success');
             console.log(JSON.stringify(data));
+
+            s(data);
           }, function(data) {
             console.log('BL init failed');
             console.log(JSON.stringify(data));
+            f(data);
           });
       },
 
@@ -197,20 +200,38 @@ angular.module('starter', ['ionic', 'controllers'])
           bluetoothSerial.connect('00:6A:8E:16:C8:EB', function(data) {
             console.log('BL connect success');
             console.log(JSON.stringify(data));
+
+            s(data);
           }, function(data) {
             console.log('BL connect failed');
             console.log(JSON.stringify(data));
 
+            f(data);
 
             // Attempt to reconnect once
             bluetoothSerial.connect('00:6A:8E:16:C8:EB', function(data) {
               console.log('reconnect succcess');
               console.log(JSON.stringify(data));
+
+              s(data);
             }, function(data) {
               console.log('reconnect failed');
               console.log(JSON.stringify(data));
+
+              f(data);
             });
           });
+      },
+
+      IsConnected: function() {
+        console.log('check is connected');
+        bluetoothSerial.isConnected(function() {
+          return true;
+        }, function() {
+          return false;
+        });
+
+
       },
 
       Write: function(data, s, f) {
@@ -228,11 +249,26 @@ angular.module('starter', ['ionic', 'controllers'])
        bluetoothSerial.read(function(data) {
         console.log('BL read success');
         console.log(JSON.stringify(data));
+
+
+          s(data);
        }, function(data) {
         console.log('BL read failed');
         console.log(JSON.stringify(data));
+
+          f(data);
        }); 
       },
+
+      EndlessRead: function(s, f) {
+        Read(function(data) {
+          s(data);
+        }, function(data) {
+          f(data);
+        });
+
+        window.setInterval(EndlessRead, 1000);
+      }
 
       /*
        * Lists paired devices
@@ -268,3 +304,317 @@ angular.module('starter', ['ionic', 'controllers'])
       }
     };
 });
+
+
+// .controller('debugCtrl', function($scope, $BLE, $compile, $BL) {
+
+//   // LIFECYCLE
+//   // ---------
+//   // initialize
+//   // scan (if device address is unknown)
+//   // connect
+//   // discover (Android) OR services/characteristics/descriptors (iOS)
+//   // read/subscribe/write characteristics/descriptors
+//   // disconnect
+//   // close
+
+//   $scope.address = '6C:71:D9:9D:64:EE';
+
+//   $scope.status = function(message) {
+//       document.getElementById('status').innerHTML = message;
+//   }
+
+//   $scope.Init = function() {
+//     $BLE.Init(function(o) {
+//       console.log('Properly connected to BLE chip');
+//       $scope.status(o.message);
+
+//       console.log(JSON.stringify(o));
+//     }, function(o) {
+//       console.log('Failed to connect to BLE chip');
+//       console.log(JSON.stringify(o));
+//     });
+//   },
+
+//   $scope.Scan = function() {
+//     // $BLE.StopScan(function(o) {
+//     //   console.log('Scan stopped successful');
+//     //   $scope.status(o.message);
+
+//     //   console.log(JSON.stringify(o));
+//     // }, function(o) {
+//     //   console.log('Scan stopped failed');
+//     //   $scope.status(o.message);
+
+//     //   console.log(JSON.stringify(o));
+//     // });
+
+//     /*
+//       {
+//         "status": "scanStarted"
+//       }
+
+//       {
+//         "status": "scanResult",
+//         "advertisement": "awArG05L",
+//         "rssi": -58,
+//         "name": "Polar H7 3B321015",
+//         "address": "ECC037FD-72AE-AFC5-9213-CA785B3B5C63"
+//       }
+//     */
+//     $BLE.StartScan(function(o) {
+
+//       document.getElementById('list').innerHTML = '';
+
+//       console.log('Scan started successful');
+//       $scope.status(o.message);
+
+//       console.log(JSON.stringify(o));
+
+//       var html = '';
+//         html += '<li>';
+//         html += '<b>Status</b> ' + o[i].status;
+//         html += ' <b>Address</b> ' + o[i].address;
+//         html += ' <b>RSSI</b> ' + o[i].rssi;
+//         html += ' <button class="connect-btn" ng-touch="AddressClick(' + o[i].address + ')" data-address="' + o[i].address + '">Connect</button>';
+//         html += '</li>';
+      
+//       document.getElementById('list').innerHTML = html;
+
+//     }, function(o) {
+//       console.log('Scan started failed');
+//       $scope.status(o.message);
+
+//       console.log(JSON.stringify(o));
+//     });
+
+//     var timer = setTimeout(function() {
+//         $scope.StopScan();
+//     }, 2000);
+//   },
+
+//   /*
+//    * Stop scanning
+//    */
+//   $scope.StopScan = function() {
+//     $BLE.StopScan(function(o) {
+//       console.log('Scan stopped');
+//     }, function(o) {
+//       console.log('Scan failed to stop');
+//     });
+//   },
+
+//   /*
+//      {
+//       "address": "00:22:D0:3B:32:10",
+//       "status": "discovered",
+//       "services": [
+//         {
+//           "characteristics": [
+//             {
+//               "descriptors": [
+
+//               ],
+//               "characteristicUuid": "2a00",
+//               "properties": {
+//                 "write": true,
+//                 "writeWithoutResponse": true,
+//                 "read": true
+//               }
+//             },
+//           ]
+//         }
+//       ]
+//     }
+//   */
+//   $scope.Discover = function(addressObj) {
+//     $BLE.Discover(function(o) {
+//       console.log('Discover successful');
+
+//       console.log(JSON.stringify(o));
+
+//       var html = '';
+//       for(item in o) {
+//         html += '<li>Name: ' + item.name + '</li>';
+//       }
+      
+//       document.getElementById('list').innerHTML = html;
+//     }, function(o) {
+//       console.log('Discover failed');
+
+//       console.log(JSON.stringify(o));
+//     }, addressObj);
+//   },
+
+  
+//     [
+//       {
+//         "name": "Polar H7 3B321015",
+//         "address": "ECC037FD-72AE-AFC5-9213-CA785B3B5C63"
+//       }
+//     ]
+   
+//   $scope.RetrieveConnected = function() {
+//     $BLE.RetrieveConnected(function(o) {
+//       console.log('Retrieve connected successful');
+//       $scope.status(o.message);
+
+//       $('#connected-list').html('');
+
+//       for(var i=0; i<o.length; i++) {
+//         $el = $('<li>' + o[i].name + ' ' + o[i].address + ' <button class="btn connect-btn" data-ng-click="AddressClick($event)" data-address="' + o[i].address + '">Select</button></li>').appendTo('#connected-list');
+//         $compile($el)($scope);
+//       }
+
+//       console.log(JSON.stringify(o));
+//     }, function(o) {
+//       console.log('Retrieve connected failed');
+//       $scope.status(o.message);
+      
+//       console.log(JSON.stringify(o));
+//     });
+//   },
+
+//   /*
+//     {
+//       "name": "Polar H7 3B321015",
+//       "address": "ECC037FD-72AE-AFC5-9213-CA785B3B5C63",
+//       "status": "connecting" / "connected" / "diconnected"
+//     }
+//    */
+
+//    // timeout toevoegen
+//   $scope.Connect = function() {
+//     var timeout = 5000;
+
+//     $BLE.Connect(function(o) {
+//       console.log('Connect successful');
+      
+//       console.log(JSON.stringify(o));
+
+//       return o;
+//     }, function(o) {
+//       console.log('Connect failed');
+      
+//       console.log(JSON.stringify(o));
+
+//       return o;
+//     }, $scope.address);
+
+//     var timer = setTimeout(function() {
+//       return false;
+//     }, timeout);
+//   },
+
+//   $scope.Disconnect = function() {
+//     $BLE.Disconnect(function(o) {
+//       console.log('Disconnect successful');
+      
+//       console.log(JSON.stringify(o));
+//     }, function(o) {
+//       console.log('Disconnect failed');
+      
+//       console.log(JSON.stringify(o));
+//     }, $scope.address);
+//   },
+
+//   $scope.Reconnect = function() {
+//     $BLE.Reconnect(function(o) {
+//       console.log('Reconnect successful');
+//       console.log(JSON.stringify(o));
+//     }, function(o) {
+//       console.log('Reconnect failed');
+
+//       console.log(JSON.stringify(o));
+//     }, $scope.address);
+//   },
+
+//   $scope.IsConnected = function(addressObj) {
+//     console.log(JSON.stringify($BLE.IsConnected(addressObj)));
+//   },
+
+//   $scope.Close = function() {
+//     $BLE.Close(function(o) {
+//       console.log('Close successful');
+      
+//       console.log(JSON.stringify(o));
+//     }, function(o) {
+//       console.log('Close failed');
+      
+//       console.log(JSON.stringify(o));
+//     }, $scope.address);
+//   },
+
+
+
+//   /*
+//    * When the connect button is pressed for an address
+//    */
+//   $scope.AddressClick = function($event) {
+//     var address = $($event.target).data('address');
+
+//     $scope.address = address;
+//   },
+
+
+
+//   $scope.InitBL = function() {
+//     $BL.Init(function(data) {
+
+//     }, function(data) {
+
+//     });
+//   },
+
+//   /*
+//    * Connect, voor button pair medewerker
+//    */
+//   $scope.ConnectBL = function() {
+//     $BL.Connect('', function(data) {
+
+//     }, function(data) {
+
+//     });
+//   },
+
+//   $scope.WriteBL = function() {
+//     $BL.Write('', function(data) {
+
+//     }, function(data) {
+
+//     });
+//   },
+
+//   $scope.ReadBL = function() {
+//     $BL.Read(function(data) {
+//       // update status van medewerker
+//     }, function(data) {
+//       // set status op beschikbaar
+//     });
+//   },
+
+//   $scope.ListBL = function() {
+//     $BL.List(function(data) {
+
+//     }, function(data) {
+
+//     });
+//   },
+
+//   $scope.SubscribeBL = function() {
+//     console.log('Subscribing...')
+//     $BL.Subscribe(function(data) {
+
+//     }, function(data) {
+
+//     });
+//   },
+
+//   $scope.DiscoverUnpairedBL = function() {
+//     $BL.DiscoverUnpaired(function(data) {
+
+//     }, function(data) {
+
+//     });
+//   }
+// })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,319 +1,55 @@
 angular.module('controllers', [])
 
-.controller('debugCtrl', function($scope, $BLE, $compile, $BL) {
+.controller('startCtrl', function($scope, $compile, $BL) {
 
-  // LIFECYCLE
-  // ---------
-  // initialize
-  // scan (if device address is unknown)
-  // connect
-  // discover (Android) OR services/characteristics/descriptors (iOS)
-  // read/subscribe/write characteristics/descriptors
-  // disconnect
-  // close
+  $scope.PairEmployee = function() {
+    // connect and subscribe to arduino
 
-  $scope.address = '6C:71:D9:9D:64:EE';
-
-  $scope.status = function(message) {
-      document.getElementById('status').innerHTML = message;
-  }
-
-  $scope.Init = function() {
-    $BLE.Init(function(o) {
-      console.log('Properly connected to BLE chip');
-      $scope.status(o.message);
-
-      console.log(JSON.stringify(o));
-    }, function(o) {
-      console.log('Failed to connect to BLE chip');
-      console.log(JSON.stringify(o));
-    });
-  },
-
-  $scope.Scan = function() {
-    // $BLE.StopScan(function(o) {
-    //   console.log('Scan stopped successful');
-    //   $scope.status(o.message);
-
-    //   console.log(JSON.stringify(o));
-    // }, function(o) {
-    //   console.log('Scan stopped failed');
-    //   $scope.status(o.message);
-
-    //   console.log(JSON.stringify(o));
-    // });
-
-    /*
-      {
-        "status": "scanStarted"
-      }
-
-      {
-        "status": "scanResult",
-        "advertisement": "awArG05L",
-        "rssi": -58,
-        "name": "Polar H7 3B321015",
-        "address": "ECC037FD-72AE-AFC5-9213-CA785B3B5C63"
-      }
-    */
-    $BLE.StartScan(function(o) {
-
-      document.getElementById('list').innerHTML = '';
-
-      console.log('Scan started successful');
-      $scope.status(o.message);
-
-      console.log(JSON.stringify(o));
-
-      var html = '';
-        html += '<li>';
-        html += '<b>Status</b> ' + o[i].status;
-        html += ' <b>Address</b> ' + o[i].address;
-        html += ' <b>RSSI</b> ' + o[i].rssi;
-        html += ' <button class="connect-btn" ng-touch="AddressClick(' + o[i].address + ')" data-address="' + o[i].address + '">Connect</button>';
-        html += '</li>';
-      
-      document.getElementById('list').innerHTML = html;
-
-    }, function(o) {
-      console.log('Scan started failed');
-      $scope.status(o.message);
-
-      console.log(JSON.stringify(o));
-    });
-
-    var timer = setTimeout(function() {
-        $scope.StopScan();
-    }, 2000);
-  },
-
-  /*
-   * Stop scanning
-   */
-  $scope.StopScan = function() {
-    $BLE.StopScan(function(o) {
-      console.log('Scan stopped');
-    }, function(o) {
-      console.log('Scan failed to stop');
-    });
-  },
-
-  /*
-     {
-      "address": "00:22:D0:3B:32:10",
-      "status": "discovered",
-      "services": [
-        {
-          "characteristics": [
-            {
-              "descriptors": [
-
-              ],
-              "characteristicUuid": "2a00",
-              "properties": {
-                "write": true,
-                "writeWithoutResponse": true,
-                "read": true
-              }
-            },
-          ]
-        }
-      ]
-    }
-  */
-  $scope.Discover = function(addressObj) {
-    $BLE.Discover(function(o) {
-      console.log('Discover successful');
-
-      console.log(JSON.stringify(o));
-
-      var html = '';
-      for(item in o) {
-        html += '<li>Name: ' + item.name + '</li>';
-      }
-      
-      document.getElementById('list').innerHTML = html;
-    }, function(o) {
-      console.log('Discover failed');
-
-      console.log(JSON.stringify(o));
-    }, addressObj);
-  },
-
-  /*
-    [
-      {
-        "name": "Polar H7 3B321015",
-        "address": "ECC037FD-72AE-AFC5-9213-CA785B3B5C63"
-      }
-    ]
-   */
-  $scope.RetrieveConnected = function() {
-    $BLE.RetrieveConnected(function(o) {
-      console.log('Retrieve connected successful');
-      $scope.status(o.message);
-
-      $('#connected-list').html('');
-
-      for(var i=0; i<o.length; i++) {
-        $el = $('<li>' + o[i].name + ' ' + o[i].address + ' <button class="btn connect-btn" data-ng-click="AddressClick($event)" data-address="' + o[i].address + '">Select</button></li>').appendTo('#connected-list');
-        $compile($el)($scope);
-      }
-
-      console.log(JSON.stringify(o));
-    }, function(o) {
-      console.log('Retrieve connected failed');
-      $scope.status(o.message);
-      
-      console.log(JSON.stringify(o));
-    });
-  },
-
-  /*
-    {
-      "name": "Polar H7 3B321015",
-      "address": "ECC037FD-72AE-AFC5-9213-CA785B3B5C63",
-      "status": "connecting" / "connected" / "diconnected"
-    }
-   */
-
-   // timeout toevoegen
-  $scope.Connect = function() {
-    var timeout = 5000;
-
-    $BLE.Connect(function(o) {
-      console.log('Connect successful');
-      
-      console.log(JSON.stringify(o));
-
-      return o;
-    }, function(o) {
-      console.log('Connect failed');
-      
-      console.log(JSON.stringify(o));
-
-      return o;
-    }, $scope.address);
-
-    var timer = setTimeout(function() {
-      return false;
-    }, timeout);
-  },
-
-  $scope.Disconnect = function() {
-    $BLE.Disconnect(function(o) {
-      console.log('Disconnect successful');
-      
-      console.log(JSON.stringify(o));
-    }, function(o) {
-      console.log('Disconnect failed');
-      
-      console.log(JSON.stringify(o));
-    }, $scope.address);
-  },
-
-  $scope.Reconnect = function() {
-    $BLE.Reconnect(function(o) {
-      console.log('Reconnect successful');
-      console.log(JSON.stringify(o));
-    }, function(o) {
-      console.log('Reconnect failed');
-
-      console.log(JSON.stringify(o));
-    }, $scope.address);
-  },
-
-  $scope.IsConnected = function(addressObj) {
-    console.log(JSON.stringify($BLE.IsConnected(addressObj)));
-  },
-
-  $scope.Close = function() {
-    $BLE.Close(function(o) {
-      console.log('Close successful');
-      
-      console.log(JSON.stringify(o));
-    }, function(o) {
-      console.log('Close failed');
-      
-      console.log(JSON.stringify(o));
-    }, $scope.address);
-  },
+    // loop read
 
 
-
-  /*
-   * When the connect button is pressed for an address
-   */
-  $scope.AddressClick = function($event) {
-    var address = $($event.target).data('address');
-
-    $scope.address = address;
-  },
-
-
-
-  $scope.InitBL = function() {
+    console.log('Pair employee');
     $BL.Init(function(data) {
 
-    }, function(data) {
+      console.log('employee init');
+      if( ! $BL.IsConnected()) {
+        console.log('employee not connected');
 
+        $BL.Connect('', function(data) {
+          console.log('employee connect');
+
+          $BL.EndlessRead();
+
+          $BL.Read(function(data) {
+            console.log('employee read');
+            // update status van medewerker
+            if(data) {
+              console.log('turn light on');
+            } else {
+              console.log('turn light off');
+            }
+          }, function(data) {
+            console.log('empolyee read fail');
+            // set status op onbeschikbaar
+          });
+        }, function(data) {
+            console.log('empolyee connect fail');
+          // connect error
+        });
+      } else {
+        console.log('empolyee connected');
+
+        $BL.EndlessRead(function() {
+
+        }, function() {
+          
+        });
+      }
+    }, function(data) {
+      // init fail
     });
+
   },
-
-  /*
-   * Connect, voor button pair medewerker
-   */
-  $scope.ConnectBL = function() {
-    $BL.Connect('', function(data) {
-
-    }, function(data) {
-
-    });
-  },
-
-  $scope.WriteBL = function() {
-    $BL.Write('', function(data) {
-
-    }, function(data) {
-
-    });
-  },
-
-  $scope.ReadBL = function() {
-    $BL.Read(function(data) {
-      // update status van medewerker
-    }, function(data) {
-      // set status op beschikbaar
-    });
-  },
-
-  $scope.ListBL = function() {
-    $BL.List(function(data) {
-
-    }, function(data) {
-
-    });
-  },
-
-  $scope.SubscribeBL = function() {
-    console.log('Subscribing...')
-    $BL.Subscribe(function(data) {
-
-    }, function(data) {
-
-    });
-  },
-
-  $scope.DiscoverUnpairedBL = function() {
-    $BL.DiscoverUnpaired(function(data) {
-
-    }, function(data) {
-
-    });
-  }
-})
-
-.controller('startCtrl', function($scope, $compile) {
 
   $scope.ToggleStatus = function() {
     var $statusBtn = $('#status-btn');
